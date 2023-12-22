6ef586a94e30c61c8f9352ccfa6c89665ef15915
R0NH0LT <111716504+R0NH0LT@users.noreply.github.com>
2023-12-21 18:29:46 -0800

    Added some JavaScript and media queries to the CSS
    Created some variables and added some media queries for hovering to make the choices more visual.

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
-// console.log("Rock Paper Scissors");
-
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
-
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 const choices = [rock, paper, scissors];
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
 
-// below is where the player and computer make their choices
+let playerScoreValue = 0;
+let computerScoreValue = 0;
+let round = 1;
+    
+function handleClick(event) {
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
 }
-// function getPlayerChoice() {
-//     return prompt("Choose rock, paper, or scissors");
-// }
 
-// below is where the player and computer's choices are saved to variables here
-
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-// below here is where the round is run
+function computerPlay() {
+    const random = Math.floor(Math.random() * 3);
+    return choices[random];
+}
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie! Try again...";
-    } else if (
-        (playerSelection === rock && computerSelection === scissors) ||
-        (playerSelection === paper && computerSelection === rock) ||
-        (playerSelection === scissors && computerSelection === paper)
-    ) {
-        return "You win!";
-    } else {
-        return "You lose!";
-    }
+    
 }
 
-// below is where the results of the games are displayed in the console for now
-
-console.log('Computer: ' + computerSelection);
-console.log('Player: ' + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
-
-// below is where the overall score is kept track of
