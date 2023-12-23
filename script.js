eba4c4f93c633d5b4677befbb38549c1d13e9325
R0NH0LT <111716504+R0NH0LT@users.noreply.github.com>
2023-12-22 19:05:05 -0800

    I have the game working but it seems there's a bug.
    Player doesn't seems to be able to win a match at all. The game is working but seems like it can only tie or let computer win.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-const choices = [rock, paper, scissors];
-const playerScore = document.querySelector('#playerScore');
-const computerScore = document.querySelector('#computerScore');
+// const rock = document.querySelector('#rock');
+// const paper = document.querySelector('#paper');
+// const scissors = document.querySelector('#scissors');
+// const choices = [rock, paper, scissors];
+const choices = document.querySelectorAll('.clickableDiv');
+const playerScoreDisplay = document.querySelector('#playerScore');
+const computerScoreDisplay = document.querySelector('#computerScore');
 
 let playerScoreValue = 0;
 let computerScoreValue = 0;
@@ -12,43 +13,71 @@ let round = 1;
 // here is the javascript for the start game button and overlay 
 const playButton = document.querySelector('#playButton');
 const overlay = document.querySelector('.overlay');
+const startGameText = document.querySelector('#startGame');
 function startGame() {
     playButton.style.display = 'none';
     overlay.style.display = 'none';
+    startGameText.style.display = 'block';
 }
 playButton.addEventListener('click', startGame);
 // here ends the javascript for the start game button and overlay
-
-function handleClick(event) {
-    const playerSelection = event.target.id;
-    const computerSelection = computerPlay();
-    const result = playRound(playerSelection, computerSelection);
-    if (result === 'You Win!') {
-        playerScoreValue++;
-    } else if (result === 'You Lose!') {
-        computerScoreValue++;
-    }
-}
 
 function computerPlay() {
     const random = Math.floor(Math.random() * 3);
     return choices[random];
 }
 
-function updateScore() {
-    
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
         return 'Tie';
     } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection === 'paper' && computerSelection === 'rock')
-    ) {
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'rock')
+    ) { 
+        playerScoreValue++;
         return 'You Win!';
     } else {
+        computerScoreValue++;
         return 'You Lose!';
     }
 }
+
+function updateScores() {
+    playerScoreDisplay.textContent = playerScoreValue;
+    computerScoreDisplay.textContent = computerScoreValue;
+}
+
+function resetScores() {
+    playerScoreValue = 0;
+    computerScoreValue = 0;
+    round = 1;
+    updateScores();
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        const playerChoice = choice.id;
+        const computerChoice = computerPlay().id;
+        const result = playRound(playerChoice, computerChoice);
+        
+        updateRoundResult(result);
+        updateScores();
+
+        if (round < 5) {
+            round++;
+            roundNumber.textContent = round;
+        } else {
+            if (playerScoreValue > computerScoreValue) {
+                updateRoundResult('You Win');
+            } else if (playerScoreValue < computerScoreValue) {
+                updateRoundResult('You Lose');
+            } else {
+                updateRoundResult('Game Tied!');
+            }
+            resetScores();
+        }
+        
+        updateScores();
+    })
+})
