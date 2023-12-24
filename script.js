86b8e796aa71eace5c3088f75f2ce7335d905de3
R0NH0LT <111716504+R0NH0LT@users.noreply.github.com>
2023-12-24 14:36:15 -0800

    Got the game fixed and better functionality.
    Got the reset button working, scoardboards working, user clicking registering properly, and movements happening more smootly.

--- script.js
+++ script.js
@@ -1,83 +1,126 @@
-// const rock = document.querySelector('#rock');
-// const paper = document.querySelector('#paper');
-// const scissors = document.querySelector('#scissors');
-// const choices = [rock, paper, scissors];
-const choices = document.querySelectorAll('.clickableDiv');
+// This is the choices below
+const rock = document.querySelector('#rock-container');
+const paper = document.querySelector('#paper-container');
+const scissors = document.querySelector('#scissors-container');
+const choicesArray = ['rock', 'paper', 'scissors'];
+// these are the score displays
 const playerScoreDisplay = document.querySelector('#playerScore');
 const computerScoreDisplay = document.querySelector('#computerScore');
+const roundNumber = document.querySelector('#round');
+const roundCounter = document.querySelector('#roundCounter');
+// the start game button, and the overlay, and the game over
+const playButton = document.querySelector('#playButton');
+const overlay = document.querySelector('.overlay');
+const gamePrompt = document.querySelector('#gamePrompt');
+const gameOver = document.querySelector('#gameOver');
+const resetButton = document.querySelector('#resetButton');
+
 
 let playerScoreValue = 0;
 let computerScoreValue = 0;
-let round = 1;
+let roundValue = 1;
 
 // here is the javascript for the start game button and overlay 
-const playButton = document.querySelector('#playButton');
-const overlay = document.querySelector('.overlay');
-const startGameText = document.querySelector('#startGame');
 function startGame() {
     playButton.style.display = 'none';
     overlay.style.display = 'none';
-    startGameText.style.display = 'block';
+    roundCounter.style.display = 'block';
+    gamePrompt.style.display = 'block';
+
+    setTimeout(() => {
+        gamePrompt.style.display = 'none';
+    }, 2000);
 }
 playButton.addEventListener('click', startGame);
 // here ends the javascript for the start game button and overlay
 
 function computerPlay() {
     const random = Math.floor(Math.random() * 3);
-    return choices[random];
+    return choicesArray[random];
 }
+console.log(computerPlay());
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return 'Tie';
+        return 'Tie!';
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper') ||
-        (playerChoice === 'paper' && computerChoice === 'rock')
-    ) { 
-        playerScoreValue++;
-        return 'You Win!';
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        playerScoreValue++;// Update player score display
+        return 'You win!';
     } else {
-        computerScoreValue++;
-        return 'You Lose!';
+        computerScoreValue++; // Update computer score display
+        return 'You lose!';
     }
 }
 
-function updateScores() {
+function updateScore() {
     playerScoreDisplay.textContent = playerScoreValue;
     computerScoreDisplay.textContent = computerScoreValue;
 }
 
-function resetScores() {
+function updateRound() {
+    if (roundValue < 5) {
+        roundValue++;
+    } else {
+        if (playerScoreValue > computerScoreValue) {
+            gamePrompt.textContent = 'You win!';
+            gameOver.style.display = 'block';
+            resetButton.style.display = 'block';
+            roundCounter.style.display = 'none';
+        } else if (playerScoreValue < computerScoreValue) {
+            gamePrompt.textContent = 'You lose!';
+            gameOver.style.display = 'block';
+            resetButton.style.display = 'block';
+            roundCounter.style.display = 'none';
+        } else {
+            gamePrompt.textContent = 'It\'s a tie!';
+            gameOver.style.display = 'block';
+            resetButton.style.display = 'block';
+            roundCounter.style.display = 'none';
+        }
+        computerPlay();
+    }
+}
+
+function resetGame() {
     playerScoreValue = 0;
     computerScoreValue = 0;
     round = 1;
-    updateScores();
+    playerScoreDisplay.textContent = playerScoreValue;
+    computerScoreDisplay.textContent = computerScoreValue;
+    resetButton.style.display = 'none';
+    roundCounter.style.display = 'block';
+    gameOver.style.display = 'none';
+    gamePrompt.style.display = 'block';
+
+    setTimeout(() => {
+        gamePrompt.style.display = 'none';
+    }, 2000);
 }
 
-choices.forEach(choice => {
-    choice.addEventListener('click', () => {
-        const playerChoice = choice.id;
-        const computerChoice = computerPlay().id;
-        const result = playRound(playerChoice, computerChoice);
-        
-        updateRoundResult(result);
-        updateScores();
-
-        if (round < 5) {
-            round++;
-            roundNumber.textContent = round;
-        } else {
-            if (playerScoreValue > computerScoreValue) {
-                updateRoundResult('You Win');
-            } else if (playerScoreValue < computerScoreValue) {
-                updateRoundResult('You Lose');
-            } else {
-                updateRoundResult('Game Tied!');
-            }
-            resetScores();
-        }
-        
-        updateScores();
-    })
+rock.addEventListener('click', () => {
+    const computerChoice = computerPlay();
+    const result = playRound('rock', computerChoice);
+    gamePrompt.textContent = result;
+    updateScore();
+    updateRound();
 })
+paper.addEventListener('click', () => {
+    const computerChoice = computerPlay();
+    const result = playRound('paper', computerChoice);
+    gamePrompt.textContent = result;
+    updateScore();
+    updateRound();
+})
+scissors.addEventListener('click', () => {
+    const computerChoice = computerPlay();
+    const result = playRound('scissors', computerChoice);
+    gamePrompt.textContent = result;
+    updateScore();
+    updateRound();
+})
+
+resetButton.addEventListener('click', resetGame);
